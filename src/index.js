70350d422cfec8aea8b8e6d30d517a00887e484d
Yash Rawat <rawatyash1999@gmail.com>
2022-02-12 21:20:53 +0530

    implemented redux basics in index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
 
+const increment = () => {
+  return {
+    type: 'INCREMENT',
+  };
+};
+const decrement = () => {
+  return {
+    type: 'DECREMENT',
+  };
+};
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+};
+let store = createStore(counter);
+
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch(increment());
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -10,6 +35,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// STORE -> GLOBALISED STATE
+// ACTION
+
+// REDUCER ->update store
+// DISPATCH --->>>fire
