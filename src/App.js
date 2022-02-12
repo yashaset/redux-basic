05dfcf4cfb94b8de7a2769cff9020b2a231211fd
Yash Rawat <rawatyash1999@gmail.com>
2022-02-12 21:04:11 +0530

    css setup clean

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'></header>
+      <header className='App-header'>
+        <h1>Hello World:)</h1>
+      </header>
     </div>
   );
 }
