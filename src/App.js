9c0896d6ff5a9cd36580c8d3f0622c19ddc7629f
Yash Rawat <rawatyash1999@gmail.com>
2022-02-12 22:22:10 +0530

    implemented use dispatch to dispatch inc and dec actions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { increment, decrement } from './actions';
+
 function App() {
   const counter = useSelector((state) => state.counterReducer);
   const isloggedIn = useSelector((state) => state.loggedReducer);
+  const dispatch = useDispatch();
+
   const privateData = <h3>You're single</h3>;
   return (
     <div className='App'>
       <header className='App-header'>
         <h1>Hello World :{counter}</h1>
+        <button
+          onClick={() => {
+            dispatch(increment());
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            dispatch(decrement());
+          }}
+        >
+          -
+        </button>
         {isloggedIn ? privateData : ''}
       </header>
     </div>
